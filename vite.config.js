e8a6421f4b6a56d6c2e6542af8042ffdb293efc1
Selomie@fkre21 <selomielamesgin23@gmail.com>
2025-11-28 21:18:41 -0800

    Apple Deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Apple-Full-Stack-Clone/", // <- add this line
+  base: "/Apple-FullStack-Clone/", // <- add this line
   plugins: [react()],
 });
